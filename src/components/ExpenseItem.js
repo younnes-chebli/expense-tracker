4df618b0e371c2449396bb9e2aff0c3cd98dbfc1
Younnes Chebli <younnesc@gmail.com>
2023-07-17 23:56:45 +0200

    feat: outputting dynamic data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
+    const expenseDate = new Date(2023, 6, 17);
+    const expenseTitle = "Car Insurance";
+    const expenseAmount = 294.67;
+
     return (
         <div className="expense-item">
-            <div>Date</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>Description</h2>
-                <div className="expense-item__price">Amount</div>
+                <h2>{expenseTitle}</h2>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     );
