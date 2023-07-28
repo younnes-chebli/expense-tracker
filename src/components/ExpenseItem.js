8ccd4750377271e9e4fc5de246ebc4935e93077b
Younnes Chebli <younnesc@gmail.com>
2023-07-28 13:29:55 +0200

    splitted components into multiple components

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,17 +1,10 @@
 import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
 
 const ExpenseItem = (props) => {
-    const month = props.expense.date.toLocaleString('en-US', {month: 'long'});
-    const day = props.expense.date.toLocaleString('en-US', {day: '2-digit'});
-    const year = props.expense.date.getFullYear();
-
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.expense.date} />
             <div className="expense-item__description">
                 <h2>{props.expense.title}</h2>
                 <div className="expense-item__price">${props.expense.amount}</div>
