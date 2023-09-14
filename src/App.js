0a390d38b63fe5c5e7a196a6a600af08ab4c9b3c
Younnes Chebli <younnesc@gmail.com>
2023-09-15 00:38:27 +0200

    child to parent component

--- src/App.js
+++ src/App.js
@@ -28,9 +28,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  }
+
   return (
     <div>
-      <NewExpense/>
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   );
