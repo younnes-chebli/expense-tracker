5e5ca76d483809cbd382845ce4c7e0a3d584605b
Younnes Chebli <younnesc@gmail.com>
2023-09-21 11:58:01 +0200

    feat(filter): working with events and states

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
+import { useState } from 'react';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2020');
+
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
         <Card className='expenses'>
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             {props.items.length === 0 ? <p className='no-expense'>No Expenses!</p> :
                 props.items.map(expense => (
                     <ExpenseItem
