6be1cb53e0fe0cc4eb0500ffca4c1996b507f0ef
Younnes Chebli <younnesc@gmail.com>
2023-10-12 00:40:16 +0200

    update

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,7 +6,7 @@ import { useState } from 'react';
 import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
-    const [filteredYear, setFilteredYear] = useState('2020');
+    const [filteredYear, setFilteredYear] = useState('2023');
 
     const filterChangeHandler = (selectedYear) => {
         setFilteredYear(selectedYear);
